094010aaf67b2e2fd20c3ef6749297b99b2f3911
waxfeetfanclub <142017582+waxfeetfanclub@users.noreply.github.com>
2023-09-28 23:11:10 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Roadmap from './components/sections/Roadmap'
 import Showcase from './components/sections/Showcase'
 import Team from './components/sections/Team'
 import Faq from './components/sections/Faq'
+import Footer from './components/Footer'
 
 
 function App() {
